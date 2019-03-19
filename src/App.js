e4c028140f20e3b7a9e3d4ce204859fa8a9973e8
Owais Abukreifeh <owais.majid@yahoo.com>
2019-03-19 13:40:50 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = { isPanelVisible: false, }
+  refPanel =  React.createRef()
+  clickHandle = () => {
+    this.setState({ isPanelVisible: ! this.state.isPanelVisible })
+  }
+  panelBlurHandle = () => { this.setState({ isPanelVisible: false }) }
+  showPanel(){
+    return this.state.isPanelVisible ?
+    <Panle
+    panelref={ this.refPanel }
+    onPanelBlur={ this.panelBlurHandle.bind(this) }
+    /> : "" 
+  }
+  
+  /**@todo REMOVE!!~! */
+  componentDidMount(){
+    if(this.state.isPanelVisible)
+      this.refPanel.current.focus();
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <button
+        className="btn-notify"
+        onClick={ this.clickHandle }
+        >
+          <i className="fas fa-bell"></i>
+        </button>
+        { this.showPanel() }
+
       </div>
     );
   }
 }
 
 export default App;
+
+
+const style = {
+  width: "200px",
+  height: "400px",
+  backgroundColor: "red"
+}
+
+
+class Panle extends Component{
+  componentDidMount(){
+    this.props.panelref.current.focus();
+  }
+  render(){
+    return (
+      <div 
+      tabIndex="123"
+      style={ style }
+      onBlur={ this.props.onPanelBlur }
+      ref={ this.props.panelref }> 
+        { this.props.children }
+      </div>
+    )
+  
+  }
+}
